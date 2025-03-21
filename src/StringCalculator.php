b5aff61026947de1f3600c50f7ff6ba332e1ed0a
miketroke <byvallees17@gmail.com>
2025-03-21 13:41:38 +0100

    [3] - Ejercicio resuleto

--- src/StringCalculator.php
+++ src/StringCalculator.php
@@ -4,5 +4,50 @@ namespace Deg540\StringCalculatorPHP;
 
 class StringCalculator
 {
-    // TODO: String Calculator Kata
+    public function add(string $number): int
+    {
+        $number = $this->getCleanCode($number);
+
+        if ($this->isMoreOfOneNumber($number)) {
+            return array_sum(array_map('intval', $number)); // Asegura que sean enteros
+        }
+
+        if ($this->isOnlyOneNumber($number)) {
+            return intval($number[0]); // Convierte explícitamente a entero
+        }
+
+        return 0;
+    }
+
+    /**
+     * @param string $number
+     * @return string[]
+     */
+    public function getCleanCode(string $number): array
+    {
+        $number = str_replace("\n", ",", $number);
+
+        $number = explode(",", $number);
+
+        return $number; // Separa la cadena por ','
+    }
+
+    /**
+     * @param array $number
+     * @return bool
+     */
+    public function isMoreOfOneNumber(array $number): bool
+    {
+        return count($number) > 1;
+    }
+
+    /**
+     * @param array $number
+     * @return bool
+     */
+    public function isOnlyOneNumber(array $number): bool
+    {
+        return count($number) == 1;
+    }
+
 }
--- tests/StringCalculatorTest.php
+++ tests/StringCalculatorTest.php
@@ -9,5 +9,68 @@ use PHPUnit\Framework\TestCase;
 
 final class StringCalculatorTest extends TestCase
 {
-    // TODO: String Calculator Kata Tests
+    /**
+     * @test
+     */
+    public function givenEmptyStringReturnsZero()
+    {
+        $stringCalculator = new StringCalculator();
+
+        $result = $stringCalculator->add("");
+
+        $this->assertEquals(0, $result);
+    }
+
+    /**
+     * @test
+     */
+    public function givenTwoNumbersReturnsSum()
+    {
+        $stringCalculator = new StringCalculator();
+
+        $result = $stringCalculator->add("1,2");
+
+        $this->assertEquals(3, $result);
+    }
+
+    /**
+     * @test
+     */
+    public function givenOneNumberReturnsNumber()
+    {
+        $stringCalculator = new StringCalculator();
+
+        $result = $stringCalculator->add("1");
+
+        $this->assertEquals(1, $result);
+    }
+
+    /**
+     * @test
+     */
+    /**
+     * @test
+     */
+    public function givenMoreThanOneNumberReturnsSum()
+    {
+        $stringCalculator = new StringCalculator();
+
+        $result = $stringCalculator->add("1,2,3");
+
+        $this->assertEquals(6, $result);
+    }
+
+    /**
+     * @test
+     */
+    public function givenMoreThanOneNumberWithLineBreakReturnsSum()
+    {
+        $stringCalculator = new StringCalculator();
+
+        $result = $stringCalculator->add("1\n2,3");
+
+        $this->assertEquals(6, $result);
+    }
+
+
 }
